083a27df4cbb214b010418d7dc09a053e01046b5
churrowaffles <106877391+churrowaffles@users.noreply.github.com>
2023-04-07 08:01:01 +0000

    Added click and hold drawing feature and slider

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
+var isDrawing = false;
+var penColor;
+
 document.addEventListener("DOMContentLoaded", function() {
-    createboxes(16)
-    smallBox = document.querySelectorAll('.small-box')
-    smallBox.forEach(e => {
-        e.addEventListener('mouseover', () => {
-            e.style.backgroundColor = 'blue';
-        })
+    penColor = getComputedStyle(document.body).getPropertyValue('--main-bg-color');
+    slider = document.querySelector('.slider');
+    sliderDisplay = document.querySelector('.slider-display');
+    updateSlider(slider.value);
+
+    slider.addEventListener("input", (e) => {
+        updateSlider(e.target.value)
     });
 })
 
-function createboxes(n) {
+function enableDrawing() {
+    // Controls mouse activity to draw in the sketch box
+    window.addEventListener("mousedown", () => isDrawing = true)
+    window.addEventListener("mouseup", () => isDrawing = false)
+    smallBox = document.querySelectorAll('.small-box')
+    smallBox.forEach(e => {
+        e.addEventListener('mouseover', () => {
+            if (isDrawing == true) {
+                e.style.backgroundColor = penColor;
+            }
+        })
+    });
+
+    // Prevents default dragging effect within the sketch box
+    outerBox = document.querySelector('.outer-box')
+    outerBox.addEventListener("mousedown", (e) =>
+        e.preventDefault()
+    )
+}
+
+
+function createInnerBox(n) {
+    // Create Boxes based on size
     innerBox = document.createElement('div')
     innerBox.classList.add('inner-box')
     for (i = 0; i < n; i++) {
         row = document.createElement('div')
         row.classList.add('row')
-        console.log(i)
         for (j = 0; j < n; j++) {
             smallBox = document.createElement('div')
             smallBox.classList.add('small-box')
@@ -23,5 +48,18 @@ function createboxes(n) {
         innerBox.appendChild(row)
     }
     outerBox = document.querySelector('.outer-box')
+    outerBox.innerHTML = ''
     outerBox.appendChild(innerBox)
+    enableDrawing();
+}
+
+
+function updateSlider(value) {
+    if (value < 1) {
+        value = 1
+    } else if (value > 50) {
+        value = 50
+    }
+    sliderDisplay.textContent = value;
+    createInnerBox(value);
 }
